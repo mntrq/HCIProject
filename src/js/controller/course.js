d144b474a86b09be0810dd7bbc5e569638eca538
Mintra <mntrqq@gmail.com>
2016-05-07 19:44:02 +0700

    toggle each course

--- src/js/controller/course.js
+++ src/js/controller/course.js
@@ -1,24 +1,21 @@
-  angular.module('courseApp', ['ui.router'])
+angular.module('courseApp', ['ui.router'])
   .controller('CourseController', function ($scope, $http) {
     var courseList = this
-    $http.get('src/json/list.json').
-    success(function(data, status, headers, config) {
+    $http.get('src/json/list.json')
+    .success(function(data, status, headers, config) {
       $scope.lists = data;
     }).
     error(function(data, status, headers, config) {
       // log error
     });
   })
-
-  $(document).ready(function () {
-    $('.nav ul li:first').addClass('active');
-    $('.tab-content:not(:first)').hide();
-    $('.nav ul li a').click(function (event) {
-        event.preventDefault();
-        var content = $(this).attr('href');
-        $(this).parent().addClass('active');
-        $(this).parent().siblings().removeClass('active');
-        $(content).show();
-        $(content).siblings('.tab-content').hide();
-    });
-});
+  .directive('collapseToggler', function(){
+  return {
+    restrict: 'A',
+    link: function(scope, elem, attrs) {
+            elem.on('click', function() {
+        $(this).siblings('.collapse').toggleClass('in');
+          });
+    }
+  };
+})
