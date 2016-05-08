cfc6d9d9956fb00aec78546926a41847c3de79e7
Mintra <mntrqq@gmail.com>
2016-05-08 23:07:34 +0700

    add registered courses to array

--- src/js/route/route.js
+++ src/js/route/route.js
@@ -2,13 +2,13 @@ angular.module('courseApp')
 .config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /home
-  $urlRouterProvider.otherwise("/home");
+  $urlRouterProvider.otherwise("/enroll");
   //
   // Now set up the states
   $stateProvider
-    .state('home', {
-      url: "/home",
-      templateUrl: "src/view/home.tmpl"
+    .state('enroll', {
+      url: "/enroll",
+      templateUrl: "src/view/enroll.tmpl"
     })
     .state('changepage', {
       url: "/changepage",
